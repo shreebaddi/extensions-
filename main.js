df373e921284329be393994271cd32d1f0abbb3c
shreebaddi <74357250+shreebaddi@users.noreply.github.com>
2022-03-27 15:39:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let btn=document.getElementById("getTimezone");
+if(btn !== null){
+    btn.addEventListener("click",Timezone);
+}
+function Timezone(){
+    let time = document.getElementById("showTimezone");
+    time.innerHTML=Date();
+}
